071476cb78a1f817ff862eeadb2c6c0746b6049c
ahmad-hawchar <hawchar.business@gmail.com>
2023-08-30 21:45:11 +0300

    fixing links

--- my-project/components/header.jsx
+++ my-project/components/header.jsx
@@ -3,37 +3,37 @@ export default function Header(props) {
   const [sidebar , setsidebar]=useState(false);
   return (
     <nav>
-      <div className="grid-cols-4   text-white h-[7vh] fixed w-[100%] top-0 backdrop-blur-sm z-10  grid align-middle">
+      <div className="grid-cols-4 border-b-2 border-cyan-400 md:border-b-0 rounded-2xl text-white h-[7vh] fixed w-[100%] top-0 backdrop-blur-sm  z-10 grid align-middle">
         <div className="ml-3 my-auto">
-          <h3>Ahmad Hawchar</h3>
+          <h3 className="sm:text-base text-sm">Ahmad Hawchar</h3>
         </div>
-        <div className="col-span-3 md:flex my-auto backdrop-blur-sm hidden">
-          <div className="ml-auto ">
+        <div className="col-span-3 md:flex my-auto  hidden">
+          <div className="ml-auto hover:border-t-cyan-400 hover:border-t-2 hover:font-semibold ">
             <a href={props.link + "/#about"}>About</a>
           </div>
           <div>
-            <a className="ml-2" href={props.link + "/#languages"}>
+            <a className="ml-2 hover:border-t-cyan-400 hover:border-t-2 hover:font-semibold" href={props.link + "/#languages"}>
               Languages
             </a>
           </div>
           <div>
-            <a className="ml-2" href={props.link + "/#projects"}>
+            <a className="ml-2 hover:border-t-cyan-400 hover:border-t-2 hover:font-semibold" href={props.link + "/#projects"}>
               My Projects
             </a>
           </div>
 
           <div>
-            <a className="mr-3 ml-2" href={props.link + "/#experiences"}>
+            <a className="mr-3 ml-2 hover:border-t-cyan-400 hover:border-t-2 hover:font-semibold" href={props.link + "/#experiences"}>
               Experience
             </a>
           </div>
         </div>
         <div
           id="hamburgermenu"
-          className="col-span-3 flex-col  backdrop-blur-sm flex md:hidden text-right  h-[100%] relative "
+          className="col-span-3 flex-col   flex md:hidden text-right  h-[100%] relative "
         >
           
-          <input type="button" value="||||" className={ sidebar?"w-[5vw] right-3 top-3 absolute hidden":"w-[5vw] right-3 top-3 absolute flex"}  onClick={ ()=>{setsidebar(!sidebar)}}></input>
+          <input type="button" value="||||" className={ sidebar?"w-[5vw] right-3 top-2 absolute hidden":"w-[5vw] right-3 top-2 absolute flex"}  onClick={ ()=>{setsidebar(!sidebar)}}></input>
           <div className={ sidebar?"flex flex-col  right-0 top-0 absolute text-start bg-cyan-400 w-18 ":"hidden"}>
           <input type="button" className="" value="X" onClick={ ()=>{setsidebar(!sidebar)}}></input>
           <div className=" ">
